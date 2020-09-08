5143daf76fef8623db5187c6ff41fa34cfe38c77
aung-khant-min <aungkhantmin@ucsm.edu.mm>
2020-09-08 20:05:41 +0630

    text detection first commit

---
+++ src/Components/TextDetection.js
@@ -0,0 +1,158 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Form = styled.form`
+    width: 70%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    @media (max-width: 450px){
+        width: 90%;
+    }
+`
+const TextArea = styled.textarea`
+    resize: vertical;
+    padding: 10px;
+    margin-top: 20px;
+    border: 1px solid #e6e6e6;
+    box-shadow: 0px 0px 5px -3px rgba(150,150,150,1);
+    border-radius: 4px;
+
+    &:hover{
+        box-shadow: 0px 0px 10px -3px rgba(150,150,150,1);
+    }
+`
+const Button = styled.button`
+    min-width: ${props => props.submit ? "120px" : "70px"};
+    margin: auto;
+    margin-top: 20px;
+    display: block;
+    border: 1px solid #e6e6e6;
+    border-radius: 4px;
+    box-shadow: 0px 0px 5px -3px rgba(150,150,150,1);
+    padding: 10px;
+    font-size: 16px;
+    background: ${props => props.close ? "white" : "linear-gradient(to bottom,#2dabf9 5%,#0688fa 100%)"};
+    letter-spacing: 1px;
+
+    &:hover{
+        box-shadow: 0px 0px 10px -3px rgba(150,150,150,1);
+    }
+    &:active{
+        position: relative;
+        top: 1px;
+    }
+`
+const Label = styled.label`
+    font-size: 17px;
+    margin-top: 10px;
+    color: #404040;
+`
+const Header = styled.h2`
+    margin: 0;
+    margin-top: 20px;
+    text-align: center;
+`
+const ModalOverlay = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    background-color: black;
+    opacity: 0.7;
+`
+const Modal = styled.div`
+    position: relative;
+	width: 250px;
+	height: auto;
+    margin: auto;
+    margin-top: -300px;
+	padding: 30px;
+	background-color: white;
+	border-radius: 4px;
+	box-shadow: 0 0 50px rgba(black,0.5);
+    z-index: 20;
+    
+    & > span{
+        text-align: center;
+        display: block;
+    }
+    & > div{
+        display: flex;
+    }
+`
+
+function TextDetection() {
+
+    const [text, setText] = React.useState('');
+    const [result, setResult] = React.useState();
+    const [showModal, setShowModal] = React.useState(false);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setText(e.target.value);
+    }
+
+    const handleCheck = (e) => {
+        e.preventDefault();
+
+        let temp = text.replace(/[\s\[\]!,*)@#%(&$_?.^"။၊]/g, '')
+            .match(/(([a-zA-Z0-9]+)|[က-အ|ဥ|ဦ](င်္|[က-အ][့း]*[်]|္[က-အ]|[ါ-ှ]){0,}|.)/g)
+
+        console.log(temp)
+
+        setResult(temp)
+        setShowModal(true)
+    }
+
+    return (
+        <div >
+
+            <Form onSubmit={e => handleCheck(e)}>
+
+                <Header>
+                    Text-Detection
+                </Header>
+
+                <Label>
+                    -Your article has to be written in Unicode format.
+                    If not, you can convert here <a href="https://www.rabbit-converter.org/Rabbit/">Rabbit Converter</a>
+                    <br />
+                    -Copy and Paste your article below & Click Check Button
+                </Label>
+
+                <TextArea rows="10" placeholder="Text..." required
+                    onChange={e => handleChange(e)} autoFocus />
+
+                <Button type="submit" submit>
+                    Check
+                </Button>
+
+                {result && showModal &&
+                    <div>
+                        <ModalOverlay onClick={e => setShowModal(false)} />
+                        <Modal className="animate__animated animate__slideInDown animate__fast">
+                            <span style={{ fontSize: "17px" }}>
+                                Word count : {result.length}
+                            </span>
+                            <div>
+                                <Button onClick={e => setShowModal(false)} close>OK</Button>
+
+                                <Button>
+                                    <a href="#" style={{ textDecoration: "none", color:"inherit" }}>
+                                        Submit Article
+                                    </a>
+                                </Button>
+                            </div>
+                        </Modal>
+                    </div>
+                }
+            </Form>
+
+        </div >
+    )
+}
+
+export default TextDetection
